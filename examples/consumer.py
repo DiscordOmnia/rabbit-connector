0aa048a57bb350ae135f146f3105ecb21856a638
vcokltfre <vcokltfre@gmail.com>
2021-09-03 06:42:59 +0100

    chore: add examples

---
+++ examples/consumer.py
@@ -0,0 +1,16 @@
+from asyncio import get_event_loop
+
+from rabbit_connector import RabbitClient
+
+
+loop = get_event_loop()
+
+async def main():
+    client = RabbitClient("amqp://guest:guest@localhost", "my_routing_key")
+
+    iterator = await client.iter_json()
+
+    async for message in iterator:
+        print(message)
+
+loop.run_until_complete(main())
---
+++ examples/producer.py
@@ -0,0 +1,18 @@
+from asyncio import get_event_loop, sleep
+
+from rabbit_connector import RabbitClient
+
+
+loop = get_event_loop()
+
+async def main():
+    client = RabbitClient("amqp://guest:guest@localhost", "my_routing_key")
+    count = 0
+
+    while True:
+        await sleep(1)
+        count += 1
+
+        await client.send({"count": count})
+
+loop.run_until_complete(main())
